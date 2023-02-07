b7f0560a82b8bafe5f9a82b980dbe34f5586b656
Elsayed Hassan <eh600039@gmail.com>
2023-02-07 18:25:58 +0200

    option chang bg

--- js/script.js
+++ js/script.js
@@ -34,12 +34,36 @@ changColor.forEach((li) => {
         e.currentTarget.classList.add("active");
     })
 })
+//change color
+let changBg = Array.from(document.querySelectorAll('.random-background > div > span'));
+//loop to change color
+changBg.forEach((span) => {
+    span.addEventListener("click", (e)=>{
+        changBg.forEach((span)=> {
+            span.classList.remove("active");
+        })
+        e.currentTarget.classList.add("active"); 
+        if(e.currentTarget.dataset.background === 'yes') {
+            bgoption = true;
+            randombg();
+        }else {
+            bgoption = false;
+            clearInterval(bgInterval);
+        }   
+    })
+})
+let bgoption = true;
+let bgInterval;
 //selesct Landing Page section
 let landingPage = document.querySelector('.landing-page');
 //images array
 let imgArray = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
 // setinterval function to change background to change image every 10s
-setInterval(()=>{
-    let randowmNum = Math.trunc(Math.random() * imgArray.length);
-    landingPage.style.backgroundImage = 'url(../img/'+imgArray[randowmNum]+')';
-},10000)
+function randombg() {
+    if(bgoption === true) {
+        bgInterval = setInterval(()=>{
+            let randowmNum = Math.trunc(Math.random() * imgArray.length);
+            landingPage.style.backgroundImage = 'url(../img/'+imgArray[randowmNum]+')';
+        },10000)
+    }
+}
